fe07e8d3fa0d7e5fbdc6b28ffc9cfd5495ab05ff
BrianNewsom <brian.newsom@colorado.edu>
2015-02-17 10:54:37 -0700

    Work on repo/repos

--- contents/js/my/repo.js
+++ contents/js/my/repo.js
@@ -2,6 +2,15 @@ function myRepo(full_name){
 
     // display a particular repo of mine in #details
     // e.g., https://api.github.com/repos/doubleshow/up
+    console.log('viewing repo')
 
-    alert('todo')
+    $.get("https://api.github.com/repos/briannewsom/" + full_name, github, function(data) {
+        var repo = data
+        $.get("/git-jquery/templates/repoList.jade", function(template) {
+            console.log(repo);
+            var html = jade.render(template, {items: repo})
+            $("#detail").html('hi')
+        })
+    })
 }
+
